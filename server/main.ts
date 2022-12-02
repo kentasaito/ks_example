c33cae5da51e7f7f6dac5896dc51be34806f5322
Kenta Saito <kenta.saito@sagittar.org>
2022-12-02 12:49:44 +0900

    2022-12-02 12:49:44

--- server/main.ts
+++ server/main.ts
@@ -3,7 +3,7 @@ import { controller } from './controller.ts';
 
 if (parseInt(Deno.env.get('USE_TLS'))) {
 	serveTls(controller.handler, {
-		port: 443,
+		port: Deno.env.get('REMOTE_PORT'),
 		certFile: '/home/deno/.getssl/' + Deno.env.get('REMOTE_FQDN') + '/' + Deno.env.get('REMOTE_FQDN') + '.crt',
 		keyFile: '/home/deno/.getssl/' + Deno.env.get('REMOTE_FQDN') + '/' + Deno.env.get('REMOTE_FQDN') + '.key',
 	});
