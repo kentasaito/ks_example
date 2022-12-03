6cf6508118eccd77fa97dba2c878dba12e58b905
Kenta Saito <kenta.saito@sagittar.org>
2022-12-03 17:31:51 +0900

    2022-12-03 17:31:51

--- server/main.ts
+++ server/main.ts
@@ -7,6 +7,12 @@ if (parseInt(Deno.env.get('USE_TLS'))) {
 		certFile: '/home/deno/.getssl/' + Deno.env.get('FQDN') + '/' + Deno.env.get('FQDN') + '.crt',
 		keyFile: '/home/deno/.getssl/' + Deno.env.get('FQDN') + '/' + Deno.env.get('FQDN') + '.key',
 	});
+	const watcher = Deno.watchFs('/home/deno/.getssl/' + Deno.env.get('FQDN') + '/' + Deno.env.get('FQDN') + '.crt');
+	for await (const event of watcher) {
+		if (event.kind === 'modify') {
+			Deno.utimeSync(new URL(import.meta.url).pathname, new Date(), new Date());
+		}
+	}
 }
 else {
 	serve(controller.handler, {
